607376760b4fd5dede3b960e78d9a4c39f5a7735
Tokuma Abdisa <tokkiyuam@gmail.com>
2024-04-04 18:59:48 +0300

    updated

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 // import http from 'http';
 dotenv.config();
+const bodyParser = require("body-parser");
 
 // Create Express app
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.json());
+app.use(bodyParser.json());
+
 // app.use(cors());
 // app.use(morgan('dev'));
 
