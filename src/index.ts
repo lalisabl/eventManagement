e8ef9bb726f88e323907b94616cbd6770fdd83ac
Lalisa <bulalalisa@gmail.com>
2024-05-13 00:20:55 -0500

    solved

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5000",
+  origin: "http://localhost:3000",
   credentials: true,
 }));
 app.use(passport.initialize());
