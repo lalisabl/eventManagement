eb45b9d376814f23ddd8de92e930311a5ef27e48
Lalisa <bulalalisa@gmail.com>
2024-05-15 23:11:43 -0500

    app created

---
+++ App/clientapp/android/app/src/main/kotlin/com/example/clientapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.clientapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ App/vendorapp/android/app/src/main/kotlin/com/example/vendorapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.vendorapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
