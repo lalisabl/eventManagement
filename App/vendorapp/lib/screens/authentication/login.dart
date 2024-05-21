6e663af4c5c5050ddbfd968b42a5da521714c1a6
firuol <firaoltesfaye81@gmail.com>
2024-05-20 21:59:44 -0700

    navigation done

---
+++ App/vendorapp/lib/screens/authentication/login.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'login!',
+              style: TextStyle(fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ App/vendorapp/lib/widgets/bottom_bar.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:vendorapp/screens/mypackages.dart';
+import 'package:vendorapp/screens/createpackage.dart';
+import 'package:vendorapp/screens/myprofile.dart';
+import 'package:vendorapp/themes/colors.dart';
+
+class OwnerBottomNavigationBar extends StatefulWidget {
+  @override
+  _OwnerBottomNavigationBarState createState() =>
+      _OwnerBottomNavigationBarState();
+}
+
+class _OwnerBottomNavigationBarState extends State<OwnerBottomNavigationBar> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    Mypackages(),
+    Createpackage(),
+    Myprofile(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: CurvedNavigationBar(
+        items: <Widget>[
+          Icon(Icons.list,
+              color:
+                  _selectedIndex == 0 ? AppColors.secondaryColor : Colors.white,
+              size: 30),
+          Icon(Icons.add,
+              color:
+                  _selectedIndex == 1 ? AppColors.secondaryColor : Colors.white,
+              size: 30),
+          Icon(Icons.person,
+              color:
+                  _selectedIndex == 2 ? AppColors.secondaryColor : Colors.white,
+              size: 30),
+        ],
+        index: _selectedIndex,
+        onTap: _onItemTapped,
+        height: 60.0,
+        color: AppColors.primaryColor,
+        buttonBackgroundColor: AppColors.primaryColor,
+        backgroundColor: Colors.white,
+        animationCurve: Curves.easeInOut,
+        animationDuration: Duration(milliseconds: 600),
+      ),
+    );
+  }
+}
