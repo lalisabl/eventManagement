89c07d714b7219b4d240029861c9b04dd6828be6
Tokuma Abdisa <tokkiyuam@gmail.com>
2024-05-23 12:22:08 +0300

    fixed

--- server/src/controllers/favoriteController.ts
+++ server/src/controllers/favoriteController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
 import { UserDocument } from '../models/user';
 const { Favorite } = require('../models/favorite');
-const { User } = require('../models/user');import Event, { validateEvent } from '../models/events';
+const { User } = require('../models/user');
+import Event, { validateEvent } from '../models/events';
 
 // Create a new favorite
 exports.createFavorite = async (req: Request, res: Response) => {
@@ -25,67 +26,7 @@ exports.createFavorite = async (req: Request, res: Response) => {
     res.status(500).json({ message: error.message });
   }
 };
-class APIfeatures {
-  query: any;
-  queryString: any;
-  constructor(query: any, queryString: any) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-  multfilter() {
-    const searchQuery = this.queryString.q || '';
-    if (typeof searchQuery === 'string') {
-      const regexSearch = {
-        $or: [
-          { title: { $regex: searchQuery, $options: 'i' } },
-          { categories: { $regex: searchQuery, $options: 'i' } },
-        ],
-      };
-      this.query.find(regexSearch);
-    }
-    return this;
-  }
-  filter() {
-    //1 build query
-    const queryObj = { ...this.queryString };
-    const excludedFields = ['page', 'limit', 'sort', 'fields', 'q'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-    // advanced query
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(
-      /\b(gte|gt|lte|lt|eq)\b/g,
-      (match) => `$${match}`
-    );
-    this.query = this.query.find(JSON.parse(queryStr));
-    return this;
-  }
-  sort() {
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort('-createdAt');
-    }
-    return this;
-  }
-  limiting() {
-    if (this.queryString.fields) {
-      const selectedFields = this.queryString.fields.split(',').join(' ');
-      this.query = this.query.select(selectedFields);
-    } else {
-      this.query = this.query.select('-__v');
-    }
-    return this;
-  }
-  paginatinating() {
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 10;
-    const skip = (page - 1) * limit;
-    this.query = this.query.skip(skip).limit(limit);
-    return this;
-  }
-}
-// Get all favorites
+
 // Get all favorites
 exports.getAllFavorites = async (req: Request, res: Response) => {
   try {
@@ -93,28 +34,7 @@ exports.getAllFavorites = async (req: Request, res: Response) => {
     const userId = req.user ? user._id : '';
     const userFavorites = await Favorite.find({ userId });
 
-    const favoritePropertyIds = new Set(
-      userFavorites.map((favorite: any) => favorite.eventId.toString())
-    );
-
-    const features = new APIfeatures(
-      Event.find({ _id: { $in: Array.from(favoritePropertyIds) } }),
-      req.query
-    )
-      .multfilter()
-      .filter()
-      .sort()
-      .limiting()
-      .paginatinating();
-
-    const favoriteProperties = await features.query;
-
-    const propertiesWithFavorites = favoriteProperties.map((property: any) => ({
-      ...Event.toObject(),
-      favorite: true,
-    }));
-
-    res.json(propertiesWithFavorites);
+    res.json(userFavorites);
   } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
