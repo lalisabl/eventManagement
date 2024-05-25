08cae3e72cdaf763208e1a28d91245df16f9f266
Lalisa <bulalalisa@gmail.com>
2024-05-25 11:11:33 -0500

    mock data replaced

--- App/organizerapp/lib/screens/profile.dart
+++ App/organizerapp/lib/screens/profile.dart
@@ -1,11 +1,48 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:organizerapp/constants/url.dart';
 import 'package:organizerapp/themes/colors.dart';
 
-class ProfileScreen extends StatelessWidget {
-  final String fullName = 'Chaka Tamirat';
-  final String email = 'chalatame.com';
-  final String phoneNumber = '0902949343';
-  final String profileImageUrl = 'https://via.placeholder.com/150'; // Default profile image URL
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  String fullName = '';
+  String email = '';
+  String phoneNumber = '';
+  String profileImageUrl = 'https://via.placeholder.com/150'; // Default profile image URL
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserProfile();
+  }
+
+  Future<void> fetchUserProfile() async {
+    final response = await http.get(
+        Uri.parse('${AppConstants.APIURL}/me'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer YOUR_TOKEN_HERE', // Replace with actual token
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> userData = jsonDecode(response.body);
+      setState(() {
+        fullName = userData['username'] ?? '';
+        email = userData['email'] ?? '';
+        phoneNumber = userData['phoneNumber'] ?? 'N/A'; // Assuming phoneNumber is part of the response
+        profileImageUrl = userData['profileImageUrl'] ?? 'https://via.placeholder.com/150'; // Assuming profileImageUrl is part of the response
+      });
+    } else {
+      // Handle error
+      print('Failed to load user profile');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +52,11 @@ class ProfileScreen extends StatelessWidget {
           padding: const EdgeInsets.only(top: 40.0), // Adding padding to move the title down
           child: Center(
             child: Text('My Profile',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),),
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ),
         toolbarHeight: 100, // Increase the height of the AppBar to accommodate the padding
@@ -99,4 +137,3 @@ class ProfileScreen extends StatelessWidget {
     );
   }
 }
-
