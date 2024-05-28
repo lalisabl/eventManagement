1bf7f11b929f74cf3f247c8c94373ee700354867
firuol <firaoltesfaye81@gmail.com>
2024-05-27 19:22:23 -0700

    ee

--- App/organizerapp/lib/screens/myevents.dart
+++ App/organizerapp/lib/screens/myevents.dart
@@ -1,21 +1,85 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:organizerapp/constants/url.dart';
+import 'package:organizerapp/models/Event.dart';
+import 'package:http/http.dart' as http;
+import 'package:cached_network_image/cached_network_image.dart';
 
-class Myevents extends StatelessWidget {
+class EventCard extends StatelessWidget {
+  final Event event;
+
+  const EventCard({Key? key, required this.event}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'packages',
-              style: TextStyle(fontSize: 24),
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              CachedNetworkImage(
+                imageUrl: 'http://localhost:5000/thumbnails/thumbnail.jpeg',
+                placeholder: (context, url) =>
+                    Center(child: CircularProgressIndicator()),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+                width: double.infinity,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+              Positioned(
+                top: 8,
+                left: 8,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    color: Colors.white,
+                    child: Text(
+                      '\$${event.vipPrice.toString()}',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  event.title,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8),
+                Text(event.description),
+                SizedBox(height: 8),
+                Text(
+                    'Date: ${event.startDate.toLocal()} - ${event.endDate.toLocal()}'),
+                SizedBox(height: 8),
+                Text('Location: ${event.location}'),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class Myevents extends StatefulWidget {
+  @override
+  _EventsListScreenState createState() => _EventsListScreenState();
+}
+
+class _EventsListScreenState extends State<Myevents> {
+  late Future<List<Event>> futureEvents;
+  @override
+  void initState() {
+    super.initState();
+    futureEvents = fetchEvents();
   }
 }
